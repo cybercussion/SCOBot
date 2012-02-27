1d8323487b7aa21de75612a39b5b499bfe35e5b6
Mark Statkus <mark@cybercussion.com>
2012-02-27 14:14:03 -0800

    removed uneeded UTC Date parsing.  Updated regex for nonUTC time.

--- js/scorm/SCOBot.js
+++ js/scorm/SCOBot.js
@@ -192,8 +192,19 @@ function SCOBot(options) {
 	 * @returns {Boolean} true or false
 	 */
 	function isISO8601UTC(v) {
-		var ISO8601Exp = /(\d{4}-[01]\d\-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+\-][0-2]\d:[0-5]\d|Z))/;
-		return ISO8601Exp.test(v);
+		var iso8601UTCExp = /(\d{4}-[01]\d\-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+\-][0-2]\d:[0-5]\d|Z))/;
+		return iso8601UTCExp.test(v);
+	}
+	/**
+	 * Is ISO 8601
+	 * I've got a RegEx to validate ISO 8601 time.
+	 * This is a great common way to do this so regardless of time zone you can reflect the 
+	 * time this time stamp was referring to.
+	 * @returns {Boolean} true or false
+	 */
+	function isISO8601(v) {
+		var iso8601Exp = /^(\d{4})-0?(\d+)-0?(\d+)[T ]0?(\d+):0?(\d+):0?(\d+)$/;
+		return iso8601Exp.test(v);
 	}
 	/**
 	 * Not Started Yet
@@ -1413,6 +1424,7 @@ function SCOBot(options) {
 	 * @returns {Boolean} true/false
 	 */
 	this.isISO8601UTC = isISO8601UTC; // Public to Private hook
+	this.isISO8601 = isISO8601;
 	/**
 	 * Set
 	 * This locally sets values local to this API
@@ -1457,55 +1469,3 @@ function SCOBot(options) {
 	$(window).bind('load', initSCO);
 	$(window).bind('unload', exitSCO);
 }
-
-/*
- * Mark: I'm leaving this bit of functionality here in case we need it.  See: https://github.com/csnover/js-iso8601
- * Jury is out right now whether we'll need to convert times back into to date times yet but there are a number of 
- * sources for this. 
- * Possibilities to save space (need testing)
- *  1. new Date(s.replace(/-/g,”/”).replace(/T/g,” “).replace(/Z/, ‘ UTC’));
- *  2.
- */
-
-/**
- * Date.parse with progressive enhancement for ISO 8601 <https://github.com/csnover/js-iso8601>
- * © 2011 Colin Snover <http://zetafleet.com>
- * Released under MIT license.
- */
-/*
-(function (Date, undefined) {
-    var origParse = Date.parse, numericKeys = [ 1, 4, 5, 6, 7, 10, 11 ];
-    Date.parse = function (date) {
-        var timestamp, struct, minutesOffset = 0;
-
-        // ES5 §15.9.4.2 states that the string should attempt to be parsed as a Date Time String Format string
-        // before falling back to any implementation-specific date parsing, so that’s what we do, even if native
-        // implementations could be faster
-        //              1 YYYY                2 MM       3 DD           4 HH    5 mm       6 ss        7 msec        8 Z 9 ±    10 tzHH    11 tzmm
-        if ((struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(date))) {
-            // avoid NaN timestamps caused by “undefined” values being passed to Date.UTC
-            for (var i = 0, k; (k = numericKeys[i]); ++i) {
-                struct[k] = +struct[k] || 0;
-            }
-
-            // allow undefined days and months
-            struct[2] = (+struct[2] || 1) - 1;
-            struct[3] = +struct[3] || 1;
-
-            if (struct[8] !== 'Z' && struct[9] !== undefined) {
-                minutesOffset = struct[10] * 60 + struct[11];
-
-                if (struct[9] === '+') {
-                    minutesOffset = 0 - minutesOffset;
-                }
-            }
-
-            timestamp = Date.UTC(struct[1], struct[2], struct[3], struct[4], struct[5] + minutesOffset, struct[6], struct[7]);
-        }
-        else {
-            timestamp = origParse ? origParse(date) : NaN;
-        }
-
-        return timestamp;
-    };
-}(Date));*/
